ba815f7ba97f2a7e300e50479e90abbbe1edfd69
watariRyo <72895473+watariRyo@users.noreply.github.com>
2024-01-03 02:13:58 +0900

    impl refresh token base

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
+	import { sendJWTToken } from "$lib/firebase/auth.client.js";
 	import authStore from "$lib/stores/auth.store";
 
     export let data;
 
     let isLoggedIn = false;
     $: isLoggedIn = $authStore.isActive ? $authStore.isLoggedIn : data.isLoggedIn;
+
+    const test = async() => {
+		await sendJWTToken()
+	}
 </script>
 
 {#if isLoggedIn}
@@ -12,3 +17,5 @@
 {:else}
     <h1>isLoggedOff</h1>
 {/if}
+
+<button on:click={test}>test</button>
